42045abe3f91e1152a355ef08df198c3acf8f2fa
achavez99 <anthonychavez199@gmail.com>
2021-02-11 23:12:04 +0100

    bien

---
+++ code/load.js
@@ -0,0 +1,5 @@
+var contenedor_loader = document.getElementById('contenedor_loader');
+setTimeout(function() {
+    contenedor_loader.getElementsByClassName.opacity = 0
+    contenedor_loader.classList.add('cerrar');
+}, 1500);
